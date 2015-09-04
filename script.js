f6dfebcb7171210039efa93b629560d600db70c9
Caio Marinho <caiomarinho8@gmail.com>
2015-09-04 10:01:50 -0300

    insert view item and script for list views

--- script.js
+++ script.js
@@ -3,4 +3,21 @@
  */
 $(document).delegate("#index", "pageinit", function(){
    $('#tab1').addClass("ui-btn-active");
+   refresh_list();
 });
+
+// esta funcao refresh lista e delega metodo de click para cada item.
+function refresh_list(){
+   for(var i=1; i<5; i++){
+      var lista = $( "#lista-"+i );
+      console.log(lista);
+      lista.promise().done(function () {
+         //refresh list here
+         $(this).listview("refresh");
+         //then add click event using delegation
+         $(this).on("click", "li", function () {
+            $.mobile.navigate('#view_item');
+         });
+      });
+   }
+}
